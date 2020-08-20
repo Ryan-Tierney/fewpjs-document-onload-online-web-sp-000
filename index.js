e5eacc5edfff3eabb5d3b81cc46e9caca6caa973
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-20 00:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+function cool() {
+  document.findElementById = "text"
+}
+
+document.addEventListener("DOMContentLoaded", function(cool) {
+  console.log("The DOM has loaded");
+  text = "This is really cool!"
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
